40bad6f27e44620f341b1ec4c64871790e66f321
Moritz Lerch <moritz@lerch-familie.de>
2021-08-07 23:37:54 +0200

    simplify example config files

--- include/example-config.h
+++ include/example-config.h
@@ -1,6 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define POWERWALL_IP_CONFIG ""
+#define POWERWALL_IP_CONFIG "replace_with_ip"
 
 #endif
--- include/example-secrets.h
+++ include/example-secrets.h
@@ -1,10 +1,10 @@
 #ifndef SECRETS_H
 #define SECRETS_H
 
-#define WIFI_SSID_SECRETS ""
-#define WIFI_PASSWORD_SECRETS ""
+#define WIFI_SSID_SECRETS "replace_with_ssid"
+#define WIFI_PASSWORD_SECRETS "replace_with_password"
 
-#define TESLA_EMAIL ""
-#define TESLA_PASSWORD ""
+#define TESLA_EMAIL "replace_with_tesla_account_email_address"
+#define TESLA_PASSWORD "replace_with_tesla_account_password"
 
 #endif
